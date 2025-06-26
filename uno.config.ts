e4e5c83227b68b1a1211568dbafd7608dda9b060
Juatos <juatos@proton.me>
2025-06-26 15:08:01 +0800

    feat: 重写 vite 配置，使用原生配置替代封装

---
+++ uno.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, presetWind4, transformerDirectives } from "unocss"
+
+export default defineConfig({
+  theme: {
+    //
+  },
+  presets: [
+    presetWind4(),
+  ],
+  transformers: [
+    transformerDirectives({
+      applyVariable: ["--at-apply"],
+    }),
+  ],
+  rules: [
+
+  ],
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,45 @@
 import { resolve } from "node:path"
-import { defineConfig } from "@rakit/conf"
+import vue from "@vitejs/plugin-vue"
+import UnoCSS from "unocss/vite"
+import AutoImport from "unplugin-auto-import/vite"
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers"
+import Components from "unplugin-vue-components/vite"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
-  root: resolve(__dirname),
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue", "vue-router", "pinia", "@vueuse/core"],
+      dts: resolve(__dirname, "src/types/auto-imports.d.ts"),
+    }),
+    Components({
+      resolvers: [
+        NaiveUiResolver(),
+        // 内联定义 RakitResolver 避免循环依赖
+        {
+          type: "component",
+          resolve: (name: string) => {
+            if (name.startsWith("Rk")) {
+              return {
+                name,
+                from: "@rakit/admin",
+              }
+            }
+          },
+        },
+      ],
+      dts: resolve(__dirname, "src/types/components.d.ts"),
+    }),
+    UnoCSS(),
+    dts({
+      tsconfigPath: resolve(__dirname, "./tsconfig.json"),
+      insertTypesEntry: true,
+      copyDtsFiles: false,
+      rollupTypes: true,
+    }),
+  ],
 
   resolve: {
     alias: {
@@ -10,15 +47,22 @@ export default defineConfig({
     },
   },
 
-  plugins: {
-    vue: true,
-    autoImport: true,
-    components: true,
-    dts: true,
-    unocss: true,
-  },
-
   build: {
-    mode: "LIB",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: () => "index.mjs",
+    },
+    rollupOptions: {
+      external: ["vue", "naive-ui"],
+      output: {
+        globals: {
+          "vue": "Vue",
+          "naive-ui": "naive",
+        },
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 })
